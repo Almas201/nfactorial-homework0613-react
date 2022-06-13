ef00b3bdb67e0f92f6547369d929ec60efcf9c17
Almas201 <m1challenge@inbox.ru>
2022-06-14 01:03:49 +0600

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import {format, formatDistance, formatRelative, subDays} from "date-fns";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const today = format(new Date(), 'dd.MM.yyyy HH:mm');
+    return (
+        <div className="App">
+            <div className="header">
+                <div id="icon">
+                    <h1 id="date">{today}</h1>
+                </div>
+            </div>
+            <div className="container">
+                <div className="img" id="first">
+                    <h1 id="word">HEY</h1>
+                </div>
+                <div className="img" id="second">
+                    <h1 id="word">LET'S</h1>
+                </div>
+                <div className="img" id="third">
+                    <h1 id="word">GIVE</h1>
+                </div>
+                <div className="img" id="four">
+                    <h1 id="word">ALL</h1>
+                </div>
+                <div className="img" id="five">
+                    <h1 id="word">YOU CAN</h1>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
